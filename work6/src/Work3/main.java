27d8b6624c2f455dfe61b1a89f626819fb45ddb4
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-07-19 22:36:33 +0300

    HW6_Babiy

---
+++ work6/src/Work3/main.java
@@ -0,0 +1,9 @@
+package Work3;
+
+public class main {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.greet("casual");
+        System.out.println(greeter);
+    }
+}
